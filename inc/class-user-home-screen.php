85d1d50b59ea427ba010275f97c6e0f895ffb6d4
Braad Martin <work@braadmartin.com>
2016-09-23 14:56:01 -0400

    More progress

--- inc/class-user-home-screen.php
+++ inc/class-user-home-screen.php
@@ -105,14 +105,42 @@ class User_Home_Screen {
 					'query_args' => array(
 						'cat' => 1575,
 					),
+					'parts' => array(
+						'publish_date',
+						'status',
+					),
 				),
 			),
 			array(
 				'type' => 'post-list',
 				'args' => array(
-					'title' => "Posts by Arielle",
+					'title' => "Posts by Braad",
 					'query_args' => array(
-						'author' => "Arielle O'Shea",
+						'author' => 306,
+					),
+					'parts' => array(
+						'status',
+					),
+				),
+			),
+			array(
+				'type' => 'post-list',
+				'args' => array(
+					'title' => "Credit Card Posts",
+					'query_args' => array(
+						'cat' => 5,
+					),
+					'parts' => array(
+						'author',
+					),
+				),
+			),
+			array(
+				'type' => 'post-list',
+				'args' => array(
+					'title' => "Posts that are Ready for CE",
+					'query_args' => array(
+						'post_status' => 'ready-for-ce',
 					),
 				),
 			),
@@ -149,24 +177,18 @@ class User_Home_Screen {
 		ob_start();
 
 		?>
-		<div id="user-home-screen-wrap">
+		<div id="user-home-screen-wrap" class="wrap" data-active-tab="main">
 			<h1><?php echo esc_html( $page_title ); ?></h1>
-			<?php
-				// If the user doesn't have widgets set up, output an initial setup prompt.
-				if ( empty( $user_widgets ) ) {
-					echo 'YOLO No Widgets';
-				}
-			?>
-			<div class="user-home-screen-widget-grid">
-				<?php
-					// If the user has widgets, output them one by one.
-					if ( ! empty( $user_widgets ) ) {
-						foreach ( $user_widgets as $widget ) {
-							echo User_Home_Screen::render_widget( $widget );
-						}
-					}
-				?>
-			</div>
+			<h2 class="nav-tab-wrapper">
+				<a class="nav-tab nav-tab-active" data-tab-id="main">
+					<?php esc_html_e( 'Main', 'user-home-screen' ); ?>
+				</a>
+				<a class="nav-tab" data-tab-id="setup">
+					<?php esc_html_e( 'Setup', 'user-home-screen' ); ?>
+				</a>
+			</h2>
+			<?php echo $this->output_main_tab( $user, $user_widgets ); ?>
+			<?php echo $this->output_setup_tab( $user, $user_widgets ); ?>
 		</div>
 		<?php
 
@@ -177,6 +199,53 @@ class User_Home_Screen {
 		 * @param  string  $screen_html  The HTML for the User Home Screen.
 		 */
 		echo apply_filters( 'user_home_screen_html', $screen_html );
+	}
+
+	/**
+	 * Output the "Main" tab contents.
+	 *
+	 * @param   WP_User  $user          The current user object.
+	 * @param   array    $user_widgets  The current user's widgets.
+	 *
+	 * @return  string                  The "Main" tab HTML.
+	 */
+	public function output_main_tab( $user, $user_widgets ) {
+
+		ob_start();
+
+		// If the user doesn't have widgets set up, output an initial setup prompt.
+		if ( empty( $user_widgets ) ) {
+			echo 'YOLO No Widgets';
+		}
+		?>
+		<div class="user-home-screen-widget-grid">
+			<?php
+				// If the user has widgets, output them one by one.
+				if ( ! empty( $user_widgets ) ) {
+					foreach ( $user_widgets as $widget ) {
+						echo User_Home_Screen::render_widget( $widget );
+					}
+				}
+			?>
+		</div>
+		<?php
+
+		$tab_html = ob_get_clean();
+		/**
+		 * Allow the HTML for the "Main" tab to be customized.
+		 *
+		 * @param  string  $tab_html  The HTML for the "Main" tab.
+		 */
+		$tab_html = apply_filters( 'user_home_screen_main_tab', $tab_html );
+
+		// Wrap the HTML in a standard wrapper.
+		$tab_html = sprintf(
+			'<div class="%s">%s</div>',
+			'user-home-screen-main-tab',
+			$tab_html
+		);
+
+		return $tab_html;
 	}
 
 	/**
@@ -231,6 +300,8 @@ class User_Home_Screen {
 			return $html;
 		}
 
+		$parts = ( ! empty( $args['parts'] ) ) ? $args['parts'] : array();
+
 		// Make the query.
 		$query = new WP_Query( $args['query_args'] );
 
@@ -253,11 +324,28 @@ class User_Home_Screen {
 
 				?>
 				<h3><?php echo esc_html( the_title() ); ?></h3>
+				<div class="user-home-screen-widget-extra-data">
+					<?php if ( in_array( 'publish_date', $parts ) ) : ?>
+					<div class="user-home-screen-widget-post-date">
+						<?php the_date(); ?>
+					</div>
+					<?php endif; ?>
+					<?php if ( in_array( 'status', $parts ) ) : ?>
+					<div class="user-home-screen-widget-post-status">
+						<?php echo esc_html( $query->post->post_status ); ?>
+					</div>
+					<?php endif; ?>
+					<?php if ( in_array( 'author', $parts ) ) : ?>
+					<div class="user-home-screen-widget-post-author">
+						<?php the_author(); ?>
+					</div>
+					<?php endif; ?>
+				</div>
 				<div class="user-home-screen-widget-action-links">
-					<a href="<?php echo esc_url( get_edit_post_link( get_the_ID(), false ) ); ?>">
+					<a href="<?php echo esc_url( get_edit_post_link( get_the_ID(), false ) ); ?>" target="_blank">
 						<?php esc_html_e( 'Edit', 'user-home-screen' ); ?>
 					</a>
-					<a href="<?php the_permalink(); ?>">
+					<a href="<?php the_permalink(); ?>" target="_blank">
 						<?php esc_html_e( 'View', 'user-home-screen' ); ?>
 					</a>
 				</div>
@@ -283,5 +371,41 @@ class User_Home_Screen {
 		);
 
 		return $html;
+	}
+
+	/**
+	 * Output the "Setup" tab contents.
+	 *
+	 * @param   WP_User  $user          The current user object.
+	 * @param   array    $user_widgets  The current user's widgets.
+	 *
+	 * @return  string                  The "Main" tab HTML.
+	 */
+	public function output_setup_tab( $user, $user_widgets ) {
+
+		ob_start();
+
+		?>
+		<form class="user-home-screen-setup-form">
+			YOLO
+		</form>
+		<?php
+
+		$tab_html = ob_get_clean();
+		/**
+		 * Allow the HTML for the "Setup" tab to be customized.
+		 *
+		 * @param  string  $tab_html  The HTML for the "Setup" tab.
+		 */
+		$tab_html = apply_filters( 'user_home_screen_setup_tab', $tab_html );
+
+		// Wrap the HTML in a standard wrapper.
+		$tab_html = sprintf(
+			'<div class="%s">%s</div>',
+			'user-home-screen-setup-tab',
+			$tab_html
+		);
+
+		return $tab_html;
 	}
 }
