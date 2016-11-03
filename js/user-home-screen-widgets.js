93923228352ba541495d2693673083bbbdf28829
Braad Martin <work@braadmartin.com>
2016-11-02 17:51:07 -0700

    Formatting

--- js/user-home-screen-widgets.js
+++ js/user-home-screen-widgets.js
@@ -195,26 +195,23 @@ var userHomeScreenWidgets = ( function( $, data ) {
 
 			$spinner.addClass( 'uhs-visible' );
 
-			$feedContent.rss(
-				feedURL,
-				{
-					limit: 10,
-					offsetStart: false,
-					offsetEnd: false,
-					ssl: true,
-					layoutTemplate: '<div class="uhs-feed-content-wrap">{entries}</div>',
-					entryTemplate: '<div class="uhs-feed-item"><div class="uhs-feed-item-left"><h3 class="uhs-feed-item-title"><a href="{url}">{title}</a></h3></div><div class="uhs-feed-item-right"><div class="uhs-feed-item-date">{date}</div><div class="uhs-feed-item-author">{author}</div></div><div class="uhs-feed-item-content">{shortBodyPlain}...</div></div>',
-					tokens: {},
-					dateFormat: 'MMM Do, YYYY',
-					error: function(){},
-					success: function(){
-						$feedContent.addClass( 'uhs-loaded' );
-					},
-					onData: function(){
-						$spinner.removeClass( 'uhs-visible' );
-					},
-				}
-			);
+			$feedContent.rss( feedURL, {
+				limit: 10,
+				offsetStart: false,
+				offsetEnd: false,
+				ssl: true,
+				layoutTemplate: '<div class="uhs-feed-content-wrap">{entries}</div>',
+				entryTemplate: '<div class="uhs-feed-item"><div class="uhs-feed-item-left"><h3 class="uhs-feed-item-title"><a href="{url}">{title}</a></h3></div><div class="uhs-feed-item-right"><div class="uhs-feed-item-date">{date}</div><div class="uhs-feed-item-author">{author}</div></div><div class="uhs-feed-item-content">{shortBodyPlain}...</div></div>',
+				tokens: {},
+				dateFormat: 'MMM Do, YYYY',
+				error: function(){},
+				success: function(){
+					$feedContent.addClass( 'uhs-loaded' );
+				},
+				onData: function(){
+					$spinner.removeClass( 'uhs-visible' );
+				},
+			});
 		});
 	};
 
